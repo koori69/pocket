23a896ab5ccebd86d7e66473dae97875b17ff10a
koori69 <koori69@users.noreply.github.com>
2019-04-09 15:27:43 +0800

    add method

--- utils.go
+++ utils.go
@@ -28,3 +28,30 @@ func UnixSecond() int64 {
 func UnixMillisecond() int64 {
 	return time.Now().UnixNano() / 1e6
 }
+
+// pow 次方
+func pow(x, n int) int32 {
+	ret := 1 // 结果初始为0次方的值，整数0次方为1。如果是矩阵，则为单元矩阵。
+	for n != 0 {
+		if n%2 != 0 {
+			ret = ret * x
+		}
+		n /= 2
+		x = x * x
+	}
+	return int32(ret)
+}
+
+// CreateCaptcha random
+func CreateCaptcha(digit int) string {
+	format := "%0" + fmt.Sprintf("%d", digit) + "v"
+	return fmt.Sprintf(format, rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(pow(10, digit)))
+}
+
+// md5 md5 32 lowercase
+func Md5(txt string) string {
+	md5Hash := md5.New()
+	io.WriteString(md5Hash, txt)
+	md5Bytes := md5Hash.Sum(nil)
+	return strings.ToLower(hex.EncodeToString(md5Bytes))
+}
